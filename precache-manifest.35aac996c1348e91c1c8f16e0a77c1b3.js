97675cf023c1ad1dc0f7d7a1c44310904f62adb5
Ahmad-Kajjan <blaketear@hotmail.com>
2020-06-09 18:55:45 -0700

    Updates

---
+++ precache-manifest.35aac996c1348e91c1c8f16e0a77c1b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4241d1976bcc56d245974628a48b5a39",
+    "url": "/airProject/index.html"
+  },
+  {
+    "revision": "6b2a25778f89a6273a1b",
+    "url": "/airProject/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "147fae23306697ff9575",
+    "url": "/airProject/static/js/2.94ff3d9a.chunk.js"
+  },
+  {
+    "revision": "0b14dac90b918c082faed0d9d1548202",
+    "url": "/airProject/static/js/2.94ff3d9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b2a25778f89a6273a1b",
+    "url": "/airProject/static/js/main.5619e5dc.chunk.js"
+  },
+  {
+    "revision": "22044d6c6aa95ac66f61",
+    "url": "/airProject/static/js/runtime-main.a8af79c2.js"
+  },
+  {
+    "revision": "d78fd1bd7c50432aae610baf18ee6315",
+    "url": "/airProject/static/media/Logo.d78fd1bd.png"
+  },
+  {
+    "revision": "10e18933e069d9ece8aaa5118e56f32f",
+    "url": "/airProject/static/media/main.10e18933.png"
+  }
+]);
